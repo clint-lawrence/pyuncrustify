d73b58a063f6f3269559d891dec124888734f128
Clint Lawrence <clint.lawrence@ampcontrolgroup.com>
2022-06-17 08:56:26 +1000

    fix the executable path on windows

--- src/pyuncrustify/__init__.py
+++ src/pyuncrustify/__init__.py
@@ -1,12 +1,17 @@
 from subprocess import run
 from pathlib import Path
+import platform
 import sys
+
 
 
 HERE = Path(__file__)
 
 def main():
-    args = [HERE.parent / "bin" / "uncrustify"]
+    if platform.system() == "Windows":
+        args = [HERE.parent / "uncrustify.exe"]
+    else:
+        args = [HERE.parent / "bin" / "uncrustify"]
     args.extend(sys.argv[1:])
 
     sys.exit(run(args).returncode)
